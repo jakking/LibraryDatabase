141bc521275674b67c8cfb37a3936a4c2a4ba388
Jake <jakking@siue.edu>
2016-04-13 14:46:44 -0500

    gitignore

--- includes/inc.db.php
+++ includes/inc.db.php
@@ -1,9 +1,9 @@
 <?php
 /* Define the four constants that can be used with either PDO or mysqli */
 DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', '434');
+DEFINE ('DB_NAME', 'cs434');
 DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PWD', '');
+DEFINE ('DB_PWD', 'jake2581');
 
 /* Define the constant for PDO use. */
 $_conString = 'mysql:host=' . DB_HOST .";dbname=" . DB_NAME;

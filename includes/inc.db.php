b8fcbaebd9363f4f3ab9409f125bbf9ad153a1ab
Jake <jakking@siue.edu>
2016-04-13 14:47:59 -0500

    reseting the db file

--- includes/inc.db.php
+++ includes/inc.db.php
@@ -3,7 +3,7 @@
 DEFINE ('DB_HOST', 'localhost');
 DEFINE ('DB_NAME', 'cs434');
 DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PWD', 'jake2581');
+DEFINE ('DB_PWD', '');
 
 /* Define the constant for PDO use. */
 $_conString = 'mysql:host=' . DB_HOST .";dbname=" . DB_NAME;
